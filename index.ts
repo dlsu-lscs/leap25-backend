3064236532575cf41ddeec9608b79e6fa019c06b
ejsadiarin <ejs@ejsadiarin.com>
2025-04-16 18:32:45 +0800

    chore: restructure for build setups

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express, { urlencoded, json } from "express";
-import "dotenv/config";
+import express, { urlencoded, json } from 'express';
+import 'dotenv/config';
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port: ${process.env.PORT}`);
 });
+
+export default app;
