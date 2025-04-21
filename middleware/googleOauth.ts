5342ffe62219411eef31d8451d5c54d4a2153736
Solenad <roe.dizon80@gmail.com>
2025-04-21 12:18:39 +0800

    feat: pool connection to mysql db

--- middleware/googleOauth.ts
+++ middleware/googleOauth.ts
@@ -6,15 +6,14 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import passport from 'passport';
 
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback", // temporary local callback
-    scope: ["profile"],
-    state: true
-  },
-  function verify(accessToken, refreshToken, profile, cb) {
-
-  }
-                    )
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback', // temporary local callback
+      scope: ['profile'],
+      state: true,
+    },
+    function verify(accessToken, refreshToken, profile, cb) {}
+  )
 );*/
