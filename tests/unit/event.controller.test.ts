f90ffe500f71bb5c6c60350667c06a03fe74f5b9
ejsadiarin <ejs@ejsadiarin.com>
2025-05-09 17:19:39 +0800

    fix: add contentful mocks for testing and update ci test workflow

--- tests/unit/event.controller.test.ts
+++ tests/unit/event.controller.test.ts
@@ -3,6 +3,55 @@ import * as EventController from '../../controllers/event.controller';
 import * as EventService from '../../services/event.service';
 
 vi.mock('../../services/event.service');
+vi.mock('../../config/contentful', () => {
+  return {
+    getContentfulEnv: vi.fn().mockResolvedValue({
+      getAsset: vi.fn().mockResolvedValue({
+        fields: {
+          file: {
+            'en-US': {
+              url: 'https://example.com/mock-image.jpg',
+            },
+          },
+        },
+      }),
+      getEntry: vi.fn().mockResolvedValue({
+        sys: {
+          id: 'mock-contentful-id',
+        },
+        fields: {
+          org_name: {
+            'en-US': 'Mock Organization',
+          },
+          org_logo: {
+            'en-US': 'https://example.com/mock-logo.jpg',
+          },
+          id: {
+            'en-US': '123',
+          },
+        },
+      }),
+    }),
+    client: {
+      getSpace: vi.fn().mockResolvedValue({
+        getEnvironment: vi.fn(),
+      }),
+    },
+  };
+});
+vi.mock('../../services/contentful.service', () => {
+  return {
+    getImageUrlById: vi
+      .fn()
+      .mockResolvedValue('https://example.com/mock-image.jpg'),
+    getOrg: vi.fn().mockResolvedValue({
+      name: 'Mock Organization',
+      org_logo: 'https://example.com/mock-logo.jpg',
+      contentful_id: 'mock-contentful-id',
+    }),
+    getSubthemeId: vi.fn().mockResolvedValue('123'),
+  };
+});
 
 const mockResponse = (): any => {
   const res: any = {};
--- tests/unit/event.service.test.ts
+++ tests/unit/event.service.test.ts
@@ -20,6 +20,57 @@ vi.mock('../../config/redis', () => ({
   isRedisReady: vi.fn().mockReturnValue(true),
 }));
 
+vi.mock('../../config/contentful', () => {
+  return {
+    getContentfulEnv: vi.fn().mockResolvedValue({
+      getAsset: vi.fn().mockResolvedValue({
+        fields: {
+          file: {
+            'en-US': {
+              url: 'https://example.com/mock-image.jpg',
+            },
+          },
+        },
+      }),
+      getEntry: vi.fn().mockResolvedValue({
+        sys: {
+          id: 'mock-contentful-id',
+        },
+        fields: {
+          org_name: {
+            'en-US': 'Mock Organization',
+          },
+          org_logo: {
+            'en-US': 'https://example.com/mock-logo.jpg',
+          },
+          id: {
+            'en-US': '123',
+          },
+        },
+      }),
+    }),
+    client: {
+      getSpace: vi.fn().mockResolvedValue({
+        getEnvironment: vi.fn(),
+      }),
+    },
+  };
+});
+
+vi.mock('../../services/contentful.service', () => {
+  return {
+    getImageUrlById: vi
+      .fn()
+      .mockResolvedValue('https://example.com/mock-image.jpg'),
+    getOrg: vi.fn().mockResolvedValue({
+      name: 'Mock Organization',
+      org_logo: 'https://example.com/mock-logo.jpg',
+      contentful_id: 'mock-contentful-id',
+    }),
+    getSubthemeId: vi.fn().mockResolvedValue('123'),
+  };
+});
+
 describe('EventService Unit Tests', () => {
   let mockDb: any;
 
