3c77abdb5db9da5094dc976af8c3f42f185c6863
Solenad <roe.dizon80@gmail.com>
2025-05-21 14:47:17 +0800

    feat: org_url in db

--- models/Org.ts
+++ models/Org.ts
@@ -3,15 +3,18 @@ export interface Org {
   name: string;
   org_logo: string;
   contentful_id: string;
+  org_url: string | undefined;
 }
 
 export interface CreateOrg {
   name: string;
   org_logo: string;
   contentful_id: string;
+  org_url?: string | undefined;
 }
 
 export interface UpdateOrg {
   name?: string;
   org_logo?: string;
+  org_url?: string | undefined;
 }
