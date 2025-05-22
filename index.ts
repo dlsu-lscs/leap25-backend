79e98a30ce33fdb5924aa2a170acf094bbf5ef01
Ej Sadiarin <106421149+ejsadiarin@users.noreply.github.com>
2025-05-22 16:28:09 +0800

    chore/config: fix rate limit (#89)
    * build: uncomment trigger-deployment
    
    * chore: config rate limit change to 5min/1000reqs
    
    * chore: prod rate limit conf

--- index.ts
+++ index.ts
@@ -54,8 +54,8 @@ const startServer = async (): Promise<void> => {
     app.use(urlencoded({ extended: true }));
     app.use(
       rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
+        windowMs: 1 * 60 * 1000, // 1 minutes
+        max: 800, // limit each IP to 800 requests per windowMs (1 min)
         standardHeaders: true,
         legacyHeaders: false,
       })
